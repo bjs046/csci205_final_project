c1f63660178841ce28698563fbbc0e7fc67d2b68
Joseph Helm <jah085@bucknell.edu>
2022-11-07 11:27:45 -0500

    "Testing what we need to do to see changes"

--- src/main/java/org/final_project/Main.java
+++ src/main/java/org/final_project/Main.java
@@ -2,6 +2,6 @@ package org.final_project;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Done");
     }
 }
