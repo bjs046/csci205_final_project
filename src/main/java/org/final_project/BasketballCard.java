9f46cae33fdc9fd573f0d7fb81e9ea94d1be074a
Junior Weil <rmw020@bucknell.edu>
2022-11-15 11:27:54 -0500

    Card Class

---
+++ src/main/java/org/final_project/BasketballCard.java
@@ -0,0 +1,27 @@
+/* *****************************************
+ * CSCI205 -Software Engineering and Design
+ * Fall2022
+ * Instructor: Prof. Brian King
+ *
+ * Name: Junior Weil
+ * Section: 02 - 11 AM
+ * Date: 11/15/22
+ * Time: 11:25 AM
+ *
+ * Project: csci205_final_project
+ * Package: org.final_project
+ * Class: BasketballCard
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package org.final_project;
+
+import java.util.ArrayList;
+
+public class BasketballCard {
+    /** static array list of overalls for basketball cards */
+    private static ArrayList<Card> overallListBasketball;
+
+}
---
+++ src/main/java/org/final_project/FootballCard.java
@@ -0,0 +1,27 @@
+/* *****************************************
+ * CSCI205 -Software Engineering and Design
+ * Fall2022
+ * Instructor: Prof. Brian King
+ *
+ * Name: Junior Weil
+ * Section: 02 - 11 AM
+ * Date: 11/15/22
+ * Time: 11:25 AM
+ *
+ * Project: csci205_final_project
+ * Package: org.final_project
+ * Class: FootballCard
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package org.final_project;
+
+import java.util.ArrayList;
+
+public class FootballCard {
+
+    /** static array list of overalls for football cards */
+    private static ArrayList<Card> overallListFootball;
+}
