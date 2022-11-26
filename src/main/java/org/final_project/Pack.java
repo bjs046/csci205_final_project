b711caa35c1e5caf098f271ae89af3a130daaf1b
Junior Weil <rmw020@bucknell.edu>
2022-11-26 14:38:03 -0600

    Card Class and child classes

---
+++ src/main/java/org/final_project/Pack.java
@@ -0,0 +1,22 @@
+/* *****************************************
+ * CSCI205 -Software Engineering and Design
+ * Fall2022
+ * Instructor: Prof. Brian King
+ *
+ * Name: Junior Weil, Joseph Helm, Brian Scotto, Brandon Cho
+ * Section: 02 - 11 AM
+ * Date: 11/26/22
+ * Time: 2:37 PM
+ *
+ * Project: csci205_final_project
+ * Package: org.final_project
+ * Class: Pack
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package org.final_project;
+
+public class Pack {
+}
