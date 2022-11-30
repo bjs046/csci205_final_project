344adebe7c1c12ff69ceb08704adcb6d275b5cf2
Junior Weil <rmw020@bucknell.edu>
2022-11-30 11:19:08 -0500

    create sort method for the SoccerCard, BasketballCard, and FootballCard

---
+++ src/main/java/org/final_project/ImportedCard.java
@@ -0,0 +1,22 @@
+/* *****************************************
+ * CSCI205 -Software Engineering and Design
+ * Fall2022
+ * Instructor: Prof. Brian King
+ *
+ * Name: Junior Weil
+ * Section: 02 - 11 AM
+ * Date: 11/30/22
+ * Time: 11:17 AM
+ *
+ * Project: csci205_final_project
+ * Package: org.final_project
+ * Class: ImportedCard
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package org.final_project;
+
+public class ImportedCard extends Card{
+}
